e5808e55bd6b97cc88d88bb2797eada3d2cca818
Vasi Philomin <vasi@cs.umd.edu>
2020-09-27 13:39:33 -0700

    Rename Integration spec to Acceptance spec

--- leaderboard/spec/acceptance_spec.rb
+++ leaderboard/spec/acceptance_spec.rb
@@ -3,18 +3,18 @@ require 'codewars_gateway'
 require 'leaderboard'
 require 'vcr_config'
 
-describe "Integration Spec", :vcr do
+describe "Acceptance Spec", :vcr do
   it "works" do
     leaderboard = Leaderboard.new
 
     expect(leaderboard.position.size).to eq(500)
     expect(leaderboard.position[1]).to eq(User.new(:name => "g964",
                                                    :clan => "None",
-                                                   :honor => 310236 
+                                                   :honor => 310355
                                                   ))
     expect(leaderboard.position[4]).to eq(User.new(:name => "jhoffner",
                                                   :clan => "Codewars",
-                                                  :honor => 139216
+                                                  :honor => 139329
                                                  ))
   end
 end
